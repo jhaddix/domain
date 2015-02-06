a2c9ee65537f3331a6280de412d616999f75e4c8
Jason Haddix <j.haddix56@gmail.com>
2015-02-06 11:01:55 -0800

    Update enumall.sh

--- enumall.sh
+++ enumall.sh
@@ -3,7 +3,7 @@
 # Subdomain enumeration script that creates/uses a dynamic resource script for recon-ng.
 # only 1 module needs api’s (/api/google_site) find instructions for that on the wiki.
 # Or you can comment out that module.
-# uses google scraping, bing scraping, baidu scraping, netcraft, and bruteforces.
+# uses google scraping, bing scraping, baidu scraping, netcraft, and bruteforces to find subdomains.
 # by @jhaddix
 
 # input from command-line becomes domain to test
